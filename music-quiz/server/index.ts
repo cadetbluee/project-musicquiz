b73943069261a5c1be4d423f53ddfa4d35f6b4b9
cadetbluee <cadetbluee@icloud.com>
2026-03-28 17:12:44 +0900

    CORS 로그

--- music-quiz/server/index.ts
+++ music-quiz/server/index.ts
@@ -10,21 +10,22 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
 
 const app = express();
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000"
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 // Next.js(3000)에서 오는 요청 허용
-app.use(cors({
-  origin: [CLIENT_URL, "http://127.0.0.1:3000", "http://localhost:3000"]
-}))
+app.use(
+  cors({
+    origin: [CLIENT_URL, "http://127.0.0.1:3000", "http://localhost:3000"],
+  })
+);
 app.use(express.json());
 
 // Express 위에 Socket.io 서버를 얹음
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: [CLIENT_URL, "http://127.0.0.1:3000", "http://localhost:3000"]
-  }
-})
-
+    origin: [CLIENT_URL, "http://127.0.0.1:3000", "http://localhost:3000"],
+  },
+});
 
 // 클라이언트가 접속했을 때
 io.on("connection", (socket) => {
@@ -39,5 +40,6 @@ io.on("connection", (socket) => {
 });
 
 httpServer.listen(4000, () => {
-  console.log("서버 실행 중: http://127.0.0.1:4000");
+  console.log("CLIENT_URL:", process.env.CLIENT_URL);
+  console.log("허용된 origin:", origin);
 });
