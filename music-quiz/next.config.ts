884fbe2ec1c83a1de6522d0dc9a410f4fd3118e2
cadetbluee <cadetbluee@icloud.com>
2026-03-28 16:11:01 +0900

    server 폴더 tsconfig에서 제외

--- music-quiz/next.config.ts
+++ music-quiz/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  webpack: (config: any) => {
-    config.externals = [...(config.externals || []), "socket.io"];
-    return config;
-  },
 };
 
 export default nextConfig;
